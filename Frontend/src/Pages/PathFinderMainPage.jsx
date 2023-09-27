4c8572da48cebf1b28d4267290d7f295f66c9463
Tahj <Tahj.Serieux46@myhunter.cuny.edu>
2023-09-27 17:45:01 -0400

    Added button to reset the map back to center

--- Frontend/src/Pages/PathFinderMainPage.jsx
+++ Frontend/src/Pages/PathFinderMainPage.jsx
@@ -1,9 +1,11 @@
 import '../Styles/PathFinderMainPage.css'
 import { useState } from 'react';
-import {GoogleMap, useLoadScript} from '@react-google-maps/api'
+import {GoogleMap, useJsApiLoader, MarkerF} from '@react-google-maps/api'
 
 import DestinationPicker from '../Components/DestinationPicker';
 const PathFinderMainPage = () => {
+    const {isLoaded} = useJsApiLoader({id: 'google-map-script',googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY})
+
     
     const showMap = false
 
@@ -16,10 +18,23 @@ const PathFinderMainPage = () => {
 
     const [origin, setOrigin] = useState(null);
     const [destination, setDestination] = useState(null);
+    const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+
+
     const handleWhereToButtonClick = () =>{
         setchoosingDestination(!choosingDestination)
     }
-    const {isLoaded} = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY})
+    // const service = new google.maps.DirectionsService()
+    // const handleMarkerClick = () =>{
+
+
+    // }
+    const request = {
+        origin: { lat: 40.712776, lng: -74.005974 },
+        destination: { lat: 34.052235, lng: -118.243683 },
+        travelMode: 'DRIVING'
+      };
+      const center = { lat: 40.7678, lng: -73.9645 };
     
 
 
@@ -29,7 +44,12 @@ const PathFinderMainPage = () => {
         <div className="PathFinderMapPage">
             <div className="main-container">
                 {showMap ? 
-                    ( isLoaded ? (<GoogleMap zoom={10} center={{ lat: 40.7678, lng: -73.9645 }} mapContainerStyle={mapContainerStyleParameter}/>) 
+                    ( isLoaded ? (
+                    <GoogleMap zoom={10} center={{ lat: 40.7678, lng: -73.9645 }} mapContainerStyle={mapContainerStyleParameter} onLoad={(map)=>setMap(map)}>
+                        <MarkerF position={{ lat:40.767957,lng:-73.964335 }} />
+                        {/* <MarkerF position={{ lat:50.767957,lng:-73.964335 }} /> */}
+                        {/* <MarkerF position={{ lat:60.767957,lng:-73.964335 }} /> */}
+                    </GoogleMap>) 
                     : (<div className='map-is-loading'>Is Loading...</div>)) 
                 : null}
 
@@ -43,6 +63,7 @@ const PathFinderMainPage = () => {
                 <div className="customize-trip">
                     {/* <label htmlFor="" className="customize-trip-button"> Customize</label> */}
                 </div>
+                <button onClick={()=> map.panTo(center)}>reset</button>
 
             </div>
         </div> 

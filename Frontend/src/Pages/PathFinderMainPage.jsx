afd01f9de07aa3ec3316bec1609b6a9b04779e6d
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-10-16 02:02:07 -0400

    new change
    new change

--- Frontend/src/Pages/PathFinderMainPage.jsx
+++ Frontend/src/Pages/PathFinderMainPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { GoogleMap, useJsApiLoader, MarkerF, DirectionsRenderer, Autocomplete } from '@react-google-maps/api';
 
 import '../Styles/PathFinderMainPage.css';
+import '../Styles/UserPreference.css';
 
 const PathFinderMainPage = () => {
   const { isLoaded } = useJsApiLoader({
@@ -214,6 +215,40 @@ const PathFinderMainPage = () => {
                 <button onClick={calculateRoute}>Calculate Route</button>
                 <button onClick={clearRoute}>Clear Route</button>
                 <button onClick={createAlbanyRoute}>Creare Albany Route</button>
+              <div className="UserPreference">
+                <div className="filter">
+                    <button className='choice' onclick="filterSelection('other')">Other</button>
+                    <button className='choice' onclick="filterSelection('food')">Food</button>
+                    <button className='choice' onclick="filterSelection('activity')">Activity</button>
+                    <button className='choice' onclick="filterSelection('shelter')">Shelter</button>
+                </div>
+                <div className='container'>
+                  <div className="type">Dietary</div>
+                    <div className='item food'>Vegatarian</div>
+                    <div className='item food'>Kosher</div>
+                    <div className='item food'>Halal</div>
+                    <div className='item food'>Low-crab</div>
+                    <div className='item food'>Dairy</div>
+                    <div className='item food'>Pescado</div>
+                    <div className='item food'>Keto</div>
+                    <div className='item food'>Vegan</div>
+                </div>
+                <div className='container'>
+                  <div className="type">Crusine</div>
+                    <div className='item food'>American</div>
+                    <div className='item food'>Asian</div>
+                    <div className='item food'>Seafood</div>
+                    <div className='item food'>Ukrain</div>
+                    <div className='item food'>Italian</div>
+                    <div className='item food'>Thai</div>
+                    <div className='item food'>Japanese</div>
+                    </div>
+                    <div>
+                    <div className='container'>Price</div>
+                    <input type='range' id="price" name="price" min="1" max="10000"></input>
+                    <input type="button" id="max" value="Set Max" onclick="msg()"></input>
+                </div>
+              </div>
             </div>
         </div>
       ) : null}

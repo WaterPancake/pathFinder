b65d0b4490c0125b210e37d8fd8aebd32c39961f
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-10-30 17:43:26 -0400

    Add files via upload

--- Frontend/src/Components/UserPreference.jsx
+++ Frontend/src/Components/UserPreference.jsx
@@ -90,7 +90,7 @@ const UserPreference = () => {
       )}
 
       <PriceBar/>
-      <button onClick={sendDataToBackend}>Submit</button>
+      <button id="submit" onClick={sendDataToBackend}>Submit</button>
     </div>
   );
 };
